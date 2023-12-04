9b68e287cfd0bae65de79fecb4e6fd802adc07b5
Ahmad Munab <ahmadmunab22@gmail.com>
2023-12-04 17:02:42 +0600

    Testing 2

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import os
 import time
-import subprocess
 
 def  update_jarvis_codebase(commit_msg, branch="munab"):
     delay = 0.1
@@ -10,12 +9,12 @@ def  update_jarvis_codebase(commit_msg, branch="munab"):
         time.sleep(delay)
         
         print("Commiting changes...")
-        os.system(f"git commit -m {commit_msg}")
+        os.system(f'git commit -m "{commit_msg}"')
         time.sleep(delay)
         
         # print("Swithing to your branch...")
         # os.system(f"git checkout -b {branch}")
-        # time.sleep(2)
+        # time.sleep(delay)
         
         print("Fetching updates...")
         os.system(f"git fetch")
@@ -26,19 +25,10 @@ def  update_jarvis_codebase(commit_msg, branch="munab"):
         time.sleep(delay)
         
         print("Pushing changes...")
-        result = subprocess.run(f"git push --force-with-lease origin {branch}", capture_output=True, text=True, shell=True)
-        output = result.stdout.strip()
-        url_start = output.find('https://')
-        url_end = output.find('.git')
-        repo_url = output[url_start:url_end + 4]
-        os.system(f"start {repo_url}")
-        return "Pushed to successfully"
+        os.system(f"git push --force-with-lease origin {branch}")
     except Exception as e:
-        return f"Error: {e}"
-    
-update_jarvis_codebase("Testing ")
+        print(f"Error: {e}")
 
 
-
-
+update_jarvis_codebase("Testing 2")
 
